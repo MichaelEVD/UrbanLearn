9889d75b07476885d09de2433c13b66cb986d653
MichaelEVD <kuzya2002@mail.ru>
2024-09-20 18:47:34 +0300

    ДЗ"Генераторные сборки"

--- 9 module/module_9_3.py
+++ 9 module/module_9_3.py
@@ -1,13 +1,9 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 full_list = list(zip(first,second))
-d = []
 first_result =(len(x[0]) - len(x[1]) for x in full_list if len(x[0]) != len(x[1]))
-for elem in first_result:
-    d.append(elem)
-print(d)
-d =[]
 second_result = (True if len(first[i]) == len(second[i]) else False for i in range(0,len(first)))
-for elem in second_result:
-    d.append(elem)
-print(d)
+
+
+print(list(first_result))
+print(list(second_result))
