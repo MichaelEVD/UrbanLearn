0beb311742831328d7701a0611c3c488d4c3cc7c
MichaelEVD <kuzya2002@mail.ru>
2024-10-25 23:55:45 +0300

    ДЗ"Логирование"

--- 12_module/module_12_4.py
+++ 12_module/module_12_4.py
@@ -31,7 +31,6 @@ class Runner:
         elif isinstance(other, Runner):
             return self.name == other.name
 
-
 class Tournament:
     def __init__(self, distance, *participants):
         self.full_distance = distance
