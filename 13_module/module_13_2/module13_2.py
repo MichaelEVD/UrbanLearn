8d37ba5938f720d199b13beba5069695ef948da7
MichaelEVD <kuzya2002@mail.ru>
2024-10-26 20:57:29 +0300

    ДЗХендлеры обработки сообщений"

---
+++ 13_module/module_13_2/module13_2.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot,Dispatcher,types,executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot,storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(massage):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_massages(massage):
+    print("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp,skip_updates=True)
